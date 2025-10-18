9f95799b3423ee846a656f6697c1e847d7a6ffb0
Selim Işık <selimoyunda6@gmail.com>
2025-10-18 23:53:30 +0300

    Login ve Register ile ilgili değişiklikler.

--- money-guard-frontend/src/main.jsx
+++ money-guard-frontend/src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import './index.css'
-import App from './App.jsx'
-import './styles/global.css'
-import { store, persistor } from './redux/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import "./styles/global.css";
+import { store, persistor } from "./redux/store.js";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
