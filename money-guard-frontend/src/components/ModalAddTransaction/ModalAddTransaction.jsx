e88c4e1aa3ef43f61e873fe6edadd323b71a239a
dilaraydmr <dilaraaydemir1@gmail.com>
2025-10-20 00:47:41 +0300

    modal klasörü düzeltildi

--- money-guard-frontend/src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ money-guard-frontend/src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,14 +1,41 @@
-const ModalAddTransaction = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+import React from 'react';
+import Modal from 'react-modal';
+import { useDispatch, useSelector } from 'react-redux';
+import s from './ModalAddTransaction.module.css';
+import AddTransactionForm from '../AddTransactionForm/AddTransactionForm';
+
+
+Modal.setAppElement('#root');
+
+function ModalAddTransaction() {
+  const dispatch = useDispatch();
+
+  const isAddOpen = useSelector(selectIsAddModalOpen);
+
   return (
-    <div>
-      <div>
-        <h3>Add Transaction</h3>
-        <p>Modal Component</p>
-        <button onClick={onClose}>Close</button>
+    <Modal
+      isOpen={isAddOpen}
+      className={s.modal}
+      overlayClassName={s.modal_Wrap}
+      onRequestClose={() => {
+        dispatch(closeAddModal());
+      }}
+    >
+      <div
+        className={s.modal_close}
+        onClick={() => {
+          dispatch(closeAddModal());
+        }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+          <path d="M1 1L17 17" stroke="#FBFBFB" />
+          <path d="M1 17L17 0.999999" stroke="#FBFBFB" />
+        </svg>
       </div>
-    </div>
+      <h2>Add transaction</h2>
+      <AddTransactionForm />
+    </Modal>
   );
-};
+}
 
 export default ModalAddTransaction;
